61e80aae93342c35c1e0710b6dd0639e5bd44044
Andrey Abakumov <andrey9864rus@mail.ru>
2022-10-01 01:42:38 +0400

    Positioned the interface elements

---
+++ LoginApp/WelcomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  WelcomeViewController.swift
+//  LoginApp
+//
+//  Created by Андрей Абакумов on 01.10.2022.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
