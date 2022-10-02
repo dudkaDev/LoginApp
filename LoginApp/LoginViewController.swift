ea66ff8c79ed8e79fa9a5b935618c1cac894dd37
Andrey Abakumov <andrey9864rus@mail.ru>
2022-10-02 13:22:22 +0400

    Final for Lesson 2.6

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -8,12 +8,54 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet var userNameTF: UITextField!
+    @IBOutlet var passwordTF: UITextField!
+    
+    let userName = "Admin"
+    let password = "Admin"
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.welcomeUser = "Welcome \(userNameTF.text ?? "")"
+        
     }
 
-
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard let _ = segue.source as? WelcomeViewController else { return }
+        userNameTF.text = ""
+        passwordTF.text = ""
+    }
+    
+    @IBAction func logInButton() {
+        if userNameTF.text == userName && passwordTF.text == password {
+            self.performSegue(withIdentifier: "Login", sender: self)
+        } else {
+            alert(title: "Login failed", message: "Please, enter correct User Name and Password")
+        }
+    }
+    
+    @IBAction func forgotUserNameButton() {
+        alert(title: "Oops!", message: "Your name is Admin")
+    }
+    
+    @IBAction func forgotPasswordButton() {
+        alert(title: "Oops!", message: "Your password is Admin")
+    }
+    
+    private func alert(title: String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true, completion: nil)
+    }
 }
 
--- LoginApp/WelcomeViewController.swift
+++ LoginApp/WelcomeViewController.swift
@@ -9,9 +9,22 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    var welcomeUser: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        welcomeLabel.text = welcomeUser
+        makesLayerGradient()
+    }
+    private func makesLayerGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [
+            UIColor.cyan.cgColor,
+            UIColor.orange.cgColor
+        ]
+        view.layer.insertSublayer(gradientLayer, at: 0)
     }
 }
